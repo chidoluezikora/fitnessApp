bc7f96cea4cebeec02c0206d004257a8814ada64
georgenaous <george.n.314@gmail.com>
2023-05-21 22:25:55 +0200

    add constraints to profilePage and add profile picture

--- profilePage/app/src/main/java/com/example/profile/MainActivity.kt
+++ profilePage/app/src/main/java/com/example/profile/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.profile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var profilePic: ImageView
+    private lateinit var addPhoto: FloatingActionButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.GRAY))
+
+        profilePic = findViewById(R.id.profilePic)
+        addPhoto = findViewById(R.id.addPhoto)
+
+        addPhoto.setOnClickListener {
+
+            ImagePicker.with(this)
+                .crop()	    			//Crop image(Optional), Check Customization for more option
+                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start()
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        profilePic.setImageURI(data?.data)
     }
 }
