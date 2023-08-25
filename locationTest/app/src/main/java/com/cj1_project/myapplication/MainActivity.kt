6d93edaf7d446490b0e2daa02ed23deab5803e79
chidoluezikora <chidozik2002@gmail.com>
2023-08-25 19:20:38 +0200

    firebase workout first try new implementation

--- locationTest/app/src/main/java/com/cj1_project/myapplication/MainActivity.kt
+++ locationTest/app/src/main/java/com/cj1_project/myapplication/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
