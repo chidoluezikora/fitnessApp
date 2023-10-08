d67d4a8fb63cc1f7a437baa09b7ee58805b717b7
chidoluezikora <chidozik2002@gmail.com>
2023-10-08 13:26:30 +0200

    redirect to user details page

---
+++ googleSignIn/app/src/main/java/com/cj1_project/googlesignin/UserModel.kt
@@ -0,0 +1,9 @@
+package com.cj1_project.googlesignin
+
+data class UserModel(
+    var userId: String,
+    var firstName: String,
+    var lastName: String,
+    var heightInCm: String,
+    var weightInKg: String
+)
